27b65658d20117c8ec4afa3966f183e9ee1bf936
Avinash2205 <avinashbanala.2205@gmail.com>
2017-10-05 15:46:07 -0500

    Update
    Description

--- Lab_Assignment-6/Source/src/app/app.module.ts
+++ Lab_Assignment-6/Source/src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { WelcomePage } from '../pages/welcome/welcome';
-import { LoginPage } from '../pages/login/login';
-import { SignupPage } from '../pages/signup/signup';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -17,9 +14,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    WelcomePage,
-    LoginPage,
-    SignupPage,
     AboutPage,
     ContactPage,
     HomePage,
@@ -32,9 +26,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    WelcomePage,
-    LoginPage,
-    SignupPage,
     AboutPage,
     ContactPage,
     HomePage,
