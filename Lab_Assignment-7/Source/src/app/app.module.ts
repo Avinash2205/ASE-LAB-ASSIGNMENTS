4c34c7449806c0b4355ce211e35368798d98ae02
Avinash2205 <avinashbanala.2205@gmail.com>
2017-10-12 12:18:39 -0500

    Update

--- Lab_Assignment-7/Source/src/app/app.module.ts
+++ Lab_Assignment-7/Source/src/app/app.module.ts
@@ -1,35 +1,16 @@
-import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-
-import { StatusBar } from '@ionic-native/status-bar';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
 import { Facebook } from '@ionic-native/facebook';
-
-
-export const firebaseConfig={
-
-  apiKey: "AIzaSyCD8_T21RDHOe6extp47AALru1eXlp0pqA",
-  authDomain: "lab-assignment-7-21525.firebaseapp.com",
-  databaseURL: "https://lab-assignment-7-21525.firebaseio.com",
-  projectId: "lab-assignment-7-21525",
-  storageBucket: "lab-assignment-7-21525.appspot.com",
-  messagingSenderId: "234148615860"
-}
-firebase.initializeApp(firebaseConfig)
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -38,16 +19,13 @@ firebase.initializeApp(firebaseConfig)
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    Facebook,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Facebook
   ]
 })
 export class AppModule {}
--- Lab_Assignment-7/Source/src/pages/home/home.ts
+++ Lab_Assignment-7/Source/src/pages/home/home.ts
@@ -1,30 +1,24 @@
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Facebook } from '@ionic-native/facebook';
-import firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  userData = null;
 
-  constructor(public navCtrl: NavController,public facebook:Facebook) {
-
+  constructor(private facebook: Facebook) {
 
   }
-fblogin(){
-  this.facebook.login(['email']).then(res=>{
-    const fc=firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken)
-    firebase.auth().signInWithCredential(fc).then(fs=>{
-      alert("firebase sec")
 
-    }).catch(ferr=>{
-      alert("firebase errc")
+  loginWithFB() {
+    this.facebook.login(['email', 'public_profile']).then((response: FacebookLoginResponse) => {
+      this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
+        this.userData = {email: profile['email'], first_name: profile['first_name'], picture: profile['picture_large']['data']['url'], username: profile['name'] };
+      })
     })
+  }
 
-  }).catch(err=>{
-    alert(JSON.stringify(err))
-  })
-}
 }
