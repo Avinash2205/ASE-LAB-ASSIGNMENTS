4a257e95cc962fc5acdd23ffb6803cff9f5c8113
Avinash2205 <avinashbanala.2205@gmail.com>
2017-11-30 13:46:53 -0600

    lab 10

--- Lab_Assignment-9/Source/Rest Service/server.js
+++ Lab_Assignment-9/Source/Rest Service/server.js
@@ -33,4 +33,4 @@ http.createServer( function (request, response) {
 }).listen(8081);
 
 // Console will print the message
-console.log('Client Server running at http://127.0.0.1:8081/');
+console.log('Client Server running at http://127.0.0.1:8081');
